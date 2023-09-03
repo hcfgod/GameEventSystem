0100d32070dc97fd763bc5dbede532b46238ca6f
hcfgod <80559377+hcfgod@users.noreply.github.com>
2023-09-03 02:29:01 -0600

    Organization

---
+++ GameEventSystem/Scripts/EventStatus/OneTimeEventStatus.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneTimeEventStatus : IEventStatus
+{
+	public bool CanTrigger(GameEvent gameEvent)
+	{
+		// Custom logic to determine if a one-time event can be triggered
+		return !gameEvent.HasBeenTriggered;
+	}
+}
